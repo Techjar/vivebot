5c47c75c52b49b932be8a5cb6ae8dc2f107dcf9e
Techjar <tecknojar@gmail.com>
2019-09-30 02:56:59 -0400

    Load token from file

--- bot.py
+++ bot.py
@@ -34,4 +34,7 @@ async def on_message(message): #budda asked for it, feel free to remove or comme
 #    if match and message.author.id != 598277022787043370 and '?bp' not in message.content.lower():
 #        await message.channel.send("{}, it sounds like the game is displaying on your monitor, but not the headset. If this is the case, may I remind you to read the FAQ before posting such inquiries. It's the first thing there!\n<http://www.vivecraft.org/faq/#troubleshooting>".format(message.author.mention))
 
-bot.run('token here')
+with open('token.txt') as f:
+    token = f.readline()
+
+bot.run(token)
