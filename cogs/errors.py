7969e6e9a26d28abc327cb286287b68bf805d07d
shay <43248357+shayypy@users.noreply.github.com>
2021-12-19 22:59:06 -0600

    Fix missing import in `errors.py`

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import sys
+import traceback
 
 
 class ErrorHandler(commands.Cog):
--- cogs/help.py
+++ cogs/help.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 import discord
-import traceback
 
 
 class Help(commands.Cog):
