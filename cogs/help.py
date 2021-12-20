5ff9c15e238e5132a354e2112642ed410c2af757
shay <43248357+shayypy@users.noreply.github.com>
2021-12-19 22:56:54 -0600

    Wrap `can_run` for `CheckFailure`s, phrasing

--- cogs/help.py
+++ cogs/help.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+import traceback
 
 
 class Help(commands.Cog):
@@ -11,10 +12,17 @@ class Help(commands.Cog):
     @commands.bot_has_permissions(embed_links=True)
     async def help_(self, ctx, *, item = None):
         """Help for cogs and commands."""
+
+        async def can_run(command):
+            try:
+                return await command.can_run(ctx)
+            except:
+                return False
+
         if not item:
             embed = discord.Embed(
                 title=f'{self.bot.user.name} Help Menu',
-                description='\n'.join(f'{cog_name} - {(cog.description or "No description").splitlines()[0]}' for cog_name, cog in self.bot.cogs.items()),
+                description='\n'.join(f'{cog_name} - {(cog.description or "No description").splitlines()[0]}' for cog_name, cog in self.bot.cogs.items() if cog.get_commands()),
                 color=0x82f4f4,
             )
 
@@ -22,7 +30,7 @@ class Help(commands.Cog):
             embed = discord.Embed(
                 title=f'Cog Help',
                 description=(
-                    '\n'.join([f'`{command.name}` - {(command.help or "No description").splitlines()[0]}' for command in cog.walk_commands() if not command.parent and (await command.can_run(ctx))])
+                    '\n'.join([f'`{command.name}` - {(command.help or "No description").splitlines()[0]}' for command in cog.walk_commands() if not command.parent and (await can_run(command))])
                 )[:4096],
                 color=0x82f4f4,
             )
@@ -34,13 +42,13 @@ class Help(commands.Cog):
                 color=0x82f4f4,
             )
             if isinstance(command, commands.Group):
-                embed.description += '\n'.join([f'`{subcommand.name}` - {(subcommand.help or "No description").splitlines()[0]}' for subcommand in command.commands if len(subcommand.parents) == 1 and (await subcommand.can_run(ctx))])
+                embed.description += '\n'.join([f'`{subcommand.name}` - {(subcommand.help or "No description").splitlines()[0]}' for subcommand in command.commands if len(subcommand.parents) == 1 and (await can_run(subcommand))])
 
             embed.description = embed.description[:4096]
             embed.add_field(name='Usage', value=f'`{ctx.prefix}{command.qualified_name} {command.signature}`')
 
         else:
-            await ctx.send('I don\'t recognize that.')
+            await ctx.send('That\'s not a cog or command.')
             return
 
         embed.set_footer(
