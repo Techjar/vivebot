9f1bccf0ac3549e17d7ae67f73ee349bdd175411
Techjar <tecknojar@gmail.com>
2023-08-31 18:19:14 -0400

    restructure

--- cogs/vivecraftfaq.py
+++ cogs/vivecraftfaq.py
@@ -131,8 +131,8 @@ class faq(commands.Cog, name='FAQ'):
                         span_acc += span - 1
             
             find_urls(2, 'client_url')
-            find_urls(4, 'spigot_url')
-            find_urls(5, 'forge_url')
+            #find_urls(4, 'spigot_url')
+            #find_urls(5, 'forge_url')
             
             response = await requests.get("https://api.modrinth.com/v2/project/vivecraft/version?loaders=[%22fabric%22]", timeout=5)
             modrinth_data = await response.json()
@@ -141,12 +141,13 @@ class faq(commands.Cog, name='FAQ'):
             embed.set_author(name="Downloads", url="http://www.vivecraft.org/downloads/", icon_url="https://media.discordapp.net/attachments/548280483809722369/621835686030475274/vc.png")
             for ver in versions:
                 field_desc = "[VR & Non-VR Client](" + ver['client_url'] + ")"
-                if 'spigot_url' in ver:
-                    field_desc += "\n[Spigot Server Plugin](" + ver['spigot_url'] + ")"
-                if 'forge_url' in ver:
-                    field_desc += "\n[Forge Server Mod](" + ver['forge_url'] + ")"
+                #if 'spigot_url' in ver:
+                #    field_desc += "\n[Spigot Server Plugin](" + ver['spigot_url'] + ")"
+                #if 'forge_url' in ver:
+                #    field_desc += "\n[Forge Server Mod](" + ver['forge_url'] + ")"
                 embed.add_field(name=ver['name'] + " Standalone", value=field_desc, inline=True)
             embed.add_field(name=modrinth_data[0]['game_versions'][-1] + " Mod", value="[Modrinth](https://modrinth.com/mod/vivecraft)\n[CurseForge](https://www.curseforge.com/minecraft/mc-mods/vivecraft)", inline=True)
+            embed.add_field(name="Server Plugins", value="[Spigot Server Plugin](https://github.com/jrbudda/Vivecraft_Spigot_Extensions/releases)\n[Forge Server Mod](https://www.curseforge.com/minecraft/mc-mods/vivecraft-forge-extensions/files)", inline=True)
             if ctx.message.reference is not None:
                 await ctx.send(embed=embed, reference=ctx.message.reference)
                 await ctx.message.delete()
